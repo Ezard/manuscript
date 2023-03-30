86dcc6bf13725a5c26ef60ef26eb8bf1cfd20e49
Ezard <ezard.ben@gmail.com>
2023-03-31 00:52:33 +0100

    feat: refactor controls to remove duplicate functions

--- manuscript/src/main/java/io/ezard/manuscript/control/BooleanControl.kt
+++ manuscript/src/main/java/io/ezard/manuscript/control/BooleanControl.kt
@@ -2,22 +2,6 @@ package io.ezard.manuscript.control
 
 import androidx.compose.material.Switch
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import io.ezard.manuscript.manuscript.ManuscriptScope
-
-@Composable
-fun ManuscriptScope.control(name: String, defaultValue: Boolean): Control<Boolean> {
-    val state = remember { mutableStateOf(defaultValue) }
-    return remember {
-        registerControl(
-            Control(
-                name = name,
-                state = state,
-            ),
-        )
-    }
-}
 
 @Composable
 internal fun BooleanControl(control: Control<Boolean>) {
--- manuscript/src/main/java/io/ezard/manuscript/control/StringControl.kt
+++ manuscript/src/main/java/io/ezard/manuscript/control/StringControl.kt
@@ -2,22 +2,6 @@ package io.ezard.manuscript.control
 
 import androidx.compose.material.TextField
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import io.ezard.manuscript.manuscript.ManuscriptScope
-
-@Composable
-fun ManuscriptScope.control(name: String, defaultValue: String): Control<String> {
-    val state = remember { mutableStateOf(defaultValue) }
-    return remember {
-        registerControl(
-            Control(
-                name = name,
-                state = state,
-            ),
-        )
-    }
-}
 
 @Composable
 internal fun StringControl(control: Control<String>) {
