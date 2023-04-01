2e9690cb06d64957c8c2b6b570f4418973133f92
Ezard <ezard.ben@gmail.com>
2023-04-01 16:04:38 +0100

    Add docs for actions

--- manuscript/src/main/java/io/ezard/manuscript/Action.kt
+++ manuscript/src/main/java/io/ezard/manuscript/Action.kt
@@ -2,25 +2,44 @@ package io.ezard.manuscript
 
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import io.ezard.manuscript.manuscript.LocalManuscriptData
+import io.ezard.manuscript.manuscript.Manuscript
 import io.ezard.manuscript.manuscript.ManuscriptScope
 import java.time.LocalDateTime
 import java.time.format.DateTimeFormatter
 
+/**
+ * A reference to an action that can be triggered by your component
+ *
+ * Call [trigger] to cause an instance of this action to appear in the "Actions" section of Manuscript's bottom sheet
+ */
 class Action(
     val name: String,
     private val onTrigger: (Action) -> Unit,
 ) {
+    /**
+     * Trigger an instance of this action to appear in the "Actions" section of Manuscript's bottom sheet
+     */
     context(ManuscriptScope)
     fun trigger() {
         onTrigger(this)
     }
 }
 
+/**
+ * Register an action for this component
+ *
+ * Calling [trigger][io.ezard.manuscript.Action.trigger] on the returned action will cause an instance of it to appear in "Actions" section of Manuscript's bottom sheet
+ *
+ * @param name the name of the action; this will be displayed to the user when the action is triggered
+ *
+ * @sample [io.ezard.manuscript.ActionSample]
+ */
 context(ManuscriptScope)
 @Composable
 fun action(name: String): Action {
@@ -38,5 +57,18 @@ internal fun Action(action: Pair<LocalDateTime, Action>) {
             style = MaterialTheme.typography.overline,
         )
         Text(text = action.second.name)
+    }
+}
+
+@Composable
+private fun ActionSample() {
+    Manuscript {
+        val onClick = action("onClick")
+
+        Variant("Button") {
+            Button(onClick = { onClick.trigger() }) {
+                Text(text = "Click me!")
+            }
+        }
     }
 }
