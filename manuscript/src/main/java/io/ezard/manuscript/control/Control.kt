601c72a4839c66fa4c5e244621daa02fb8a61d0a
Ezard <ezard.ben@gmail.com>
2023-04-01 16:27:23 +0100

    Add docs for controls

--- manuscript/src/main/java/io/ezard/manuscript/control/Control.kt
+++ manuscript/src/main/java/io/ezard/manuscript/control/Control.kt
@@ -1,16 +1,22 @@
 package io.ezard.manuscript.control
 
 import androidx.compose.foundation.layout.*
+import androidx.compose.material.Button
 import androidx.compose.material.Text
 import androidx.compose.material.TextFieldDefaults
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
+import io.ezard.manuscript.Variant
 import io.ezard.manuscript.manuscript.LocalManuscriptData
+import io.ezard.manuscript.manuscript.Manuscript
 import io.ezard.manuscript.manuscript.ManuscriptScope
 import kotlin.reflect.KProperty
 
+/**
+ * A reference to a control for a component
+ */
 class Control<T>(
     val name: String,
     internal val state: MutableState<T>,
@@ -35,6 +41,25 @@ internal fun <T> Control(control: Control<T>, block: @Composable BoxScope.() -> 
     }
 }
 
+/**
+ * Register a control for this component
+ *
+ * This allows the user to change the data that the component is using, and see updates to it in realtime
+ *
+ * Currently supported data types for controls are:
+ * - Boolean
+ * - Double
+ * - Float
+ * - Int
+ * - String
+ *
+ * If a control with an unsupported data type is registered then it will not appear in Manuscript's bottom sheet, and the value of the control will always be the default value
+ *
+ * @param [name] the name of the control; this will be displayed alongside the input field in Manuscript's bottom sheet
+ * @param [defaultValue] the initial value of the control
+ *
+ * @sample [ControlSample]
+ */
 context(ManuscriptScope)
 @Composable
 fun <T> control(name: String, defaultValue: T): Control<T> {
@@ -47,5 +72,18 @@ fun <T> control(name: String, defaultValue: T): Control<T> {
                 state = state,
             ),
         )
+    }
+}
+
+@Composable
+private fun ControlSample() {
+    Manuscript {
+        val text by control(name = "Text", defaultValue = "Click me!")
+
+        Variant(name = "Button") {
+            Button(onClick = {}) {
+                Text(text = text)
+            }
+        }
     }
 }
