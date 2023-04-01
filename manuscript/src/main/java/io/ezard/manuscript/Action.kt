2b4ba2b81192797d8400cd389c1212615efd3c20
Ezard <ezard.ben@gmail.com>
2023-04-01 16:24:48 +0100

    Tweak doc

--- manuscript/src/main/java/io/ezard/manuscript/Action.kt
+++ manuscript/src/main/java/io/ezard/manuscript/Action.kt
@@ -14,7 +14,7 @@ import java.time.LocalDateTime
 import java.time.format.DateTimeFormatter
 
 /**
- * A reference to an action that can be triggered by your component
+ * A reference to an action that can be triggered by a component
  *
  * Call [trigger] to cause an instance of this action to appear in the "Actions" section of Manuscript's bottom sheet
  */
