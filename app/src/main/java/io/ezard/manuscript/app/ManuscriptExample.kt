32a97bc7c308e98e57cca28b8c04de003730cc11
Ezard <ezard.ben@gmail.com>
2023-04-09 03:07:15 +0100

    Add manuscript example

---
+++ app/src/main/java/io/ezard/manuscript/app/ManuscriptExample.kt
@@ -0,0 +1,31 @@
+package io.ezard.manuscript.app
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import io.ezard.manuscript.actions.action
+import io.ezard.manuscript.controls.control
+import io.ezard.manuscript.manuscript.Manuscript
+import io.ezard.manuscript.variant.Variant
+
+@Preview
+@Composable
+fun ButtonManuscript() {
+    Manuscript {
+        // create controls to allow data to be changed in realtime when viewing the component
+        val text by control(name = "Text", defaultValue = "Click me!")
+
+        // create actions to register when certain interactions with the component occur
+        val onClick = action(name = "onClick")
+
+        // set up 1 or more variants of the component
+        Variant(name = "Red") {
+            RedButton(text = text, onClick = { onClick.trigger() })
+        }
+        Variant(name = "Green") {
+            GreenButton(text = text, onClick = { onClick.trigger() })
+        }
+        Variant(name = "Blue") {
+            BlueButton(text = text, onClick = { onClick.trigger() })
+        }
+    }
+}
